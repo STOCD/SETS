53a915e4388ff40263340751a37d31c858fc8e56
Stephen Hill <nehpets@zosimple.net>
2023-04-04 17:44:41 -0700

    Packaging prep
    Changes should not affect current installs, just secondary file locations for future changes.

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name='SETS',
+    version='20230401a_beta',
+    packages=[''],
+    url='https://stobuilds.com/SETS/',
+    license='GNU General Public License v3.0',
+    author='',
+    author_email='',
+    description='A Star Trek Online build tool in Python'
+)
+
+if __name__ == "__main__":
+    setup()
---
+++ src/PreApp/__init__.py
@@ -0,0 +1,1 @@
+import src.PreApp.snapshot
