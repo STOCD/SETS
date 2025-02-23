208e3fda901f2bcee63535df41a6c8e9fd0aa117
Shinga13 <93780215+Shinga13@users.noreply.github.com>
2025-02-23 20:50:26 +0100

    added building process!

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from src import SETS
 
 class Launcher():
 
-    version = '2025.02b237'
+    version = '2025.02b238'
     __version__ = '2.0'
 
     # holds the style of the app
@@ -533,9 +533,9 @@ class Launcher():
     @staticmethod
     def base_path() -> str:
         """initialize the base path"""
-        try:
-            base_path = sys._MEIPASS
-        except Exception:
+        if getattr(sys, 'frozen', False):
+            base_path = os.path.dirname(sys.executable)
+        else:
             base_path = os.path.abspath(os.path.dirname(__file__))
         return base_path
 
